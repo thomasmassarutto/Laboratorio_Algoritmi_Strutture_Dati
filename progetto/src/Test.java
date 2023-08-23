7c417336444afe7d38275aae035086a2e8f410bb
thomasmassarutto <Thomas.massarutto@gmail.com>
2023-08-23 11:08:10 +0200

    Test Elearning

---
+++ progetto/src/Test.java
@@ -0,0 +1,28 @@
+/*
+ * Classe Test:
+ * 
+ * Questa classe permette di testare gli algoritmi su Elearning.
+ * 
+ * Commenta o decommenta le righe fractionalperiod = Algos.period...(s);
+ * in base all'algoritmo che vuoi testare.
+ * 
+ * NB: carica Test come classe principale e Algos.
+ */
+
+import java.util.Scanner;
+public class Test {
+
+    public static void main(String[] args) {
+        int fractionalperiod=0;
+
+        Scanner stringa = new Scanner(System.in);
+        String s = stringa.nextLine();
+
+        //fractionalperiod = Algos.periodNaive(s);
+
+        //fractionalperiod = Algos.periodSmart(s);
+
+        System.out.println(fractionalperiod);
+
+    }
+}
