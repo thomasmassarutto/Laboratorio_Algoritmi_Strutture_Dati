e85483ae459382e13daedecc2f2b0c0d2bfd6de4
thomasmassarutto <Thomas.massarutto@gmail.com>
2023-08-25 11:07:45 +0200

    es 25

--- es25/src/es25.java
+++ es25/src/es25.java
@@ -1,5 +1,32 @@
-public class es25 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World 25");
+import java.util.Scanner;
+import static java.lang.Math.max;
+
+public class Es25 {
+    public static int editing(String stringa1, String stringa2){
+        int n = stringa1.length();
+        int m = stringa2.length();
+
+        int[][] matrice = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0) {
+                    matrice[i][j] = j;
+                } else if (j == 0) {
+                    matrice[i][j] = i;
+                } else if (stringa1.charAt(i - 1) == stringa2.charAt(j - 1)) {
+                    matrice[i][j] = matrice[i - 1][j - 1];
+                } else {
+                    matrice[i][j] = 1 + Math.min(Math.min(matrice[i - 1][j], matrice[i][j - 1]), matrice[i - 1][j - 1]);
+                }
+            }
+        }
+        return matrice[n][m];
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String stringa1 = input.nextLine();
+        String stringa2 = input.nextLine();
+        System.out.print(editing(stringa1, stringa2));
     }
 }
